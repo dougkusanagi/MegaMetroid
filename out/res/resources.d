ce2b76d442b974d2238d72cd68f156943eb4dded
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-01-05 12:16:18 -0300

    compiled resources

---
+++ out/res/resources.d
@@ -0,0 +1,5 @@
+out/res/resources.o: res/resources.res \
+D:/MegaDriveProjects/Metroid/SGDK_180+/MegaMetroidGithub/res/images/samus_defaul_suit.png \
+D:/MegaDriveProjects/Metroid/SGDK_180+/MegaMetroidGithub/res/images/bg.png \
+D:/MegaDriveProjects/Metroid/SGDK_180+/MegaMetroidGithub/res/images/level-original.png \
+D:/MegaDriveProjects/Metroid/SGDK_180+/MegaMetroidGithub/res/sound/jump.wav
---
+++ out/src/physics.d
@@ -0,0 +1,14 @@
+out/src/physics.o: src/physics.c src/../inc/physics.h \
+ D:/sgdk/inc/genesis.h D:/sgdk/inc/types.h D:/sgdk/inc/config.h \
+ D:/sgdk/inc/asm.h D:/sgdk/inc/sys.h D:/sgdk/inc/sram.h \
+ D:/sgdk/inc/mapper.h D:/sgdk/inc/memory.h D:/sgdk/inc/memory_base.h \
+ D:/sgdk/inc/tools.h D:/sgdk/inc/bmp.h D:/sgdk/inc/maths.h \
+ D:/sgdk/inc/vdp.h D:/sgdk/inc/pal.h D:/sgdk/inc/dma.h \
+ D:/sgdk/inc/vdp_tile.h D:/sgdk/inc/vdp_bg.h D:/sgdk/inc/map.h \
+ D:/sgdk/inc/pool.h D:/sgdk/inc/object.h D:/sgdk/inc/font.h \
+ D:/sgdk/res/libres.h D:/sgdk/inc/string.h D:/sgdk/inc/tab_cnv.h \
+ D:/sgdk/inc/maths3D.h D:/sgdk/inc/vdp_spr.h D:/sgdk/inc/vram.h \
+ D:/sgdk/inc/sprite_eng.h D:/sgdk/inc/sound.h D:/sgdk/inc/xgm.h \
+ D:/sgdk/inc/z80_ctrl.h D:/sgdk/inc/ym2612.h D:/sgdk/inc/psg.h \
+ D:/sgdk/inc/joy.h D:/sgdk/inc/timer.h D:/sgdk/inc/task.h \
+ D:/sgdk/inc/task_cst.h src/../inc/types.h
